65c4570294ecc88133a65669961a8bc60e046075
wael2000 <weldoami@redhat.com>
2023-12-02 10:08:40 +0100

    fixes

--- containers/00-scripts/minimal/05-pod-load-testing.sh
+++ containers/00-scripts/minimal/05-pod-load-testing.sh
@@ -3,4 +3,5 @@ podman pod create --name load-pod
 for i in {1..10}
 do
    podman run --pod=load-pod --name hellogo$i -d -e SEQ=$i hellogo 
+   podman container inspect hellogo$i | jq '.[].NetworkSettings.IPAddress'
 done
